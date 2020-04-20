757d4fc6d4c7e8f40d092cc6dc26e0226856440c
Noman Hamlani <noman.hamlani@gmail.com>
2020-04-20 12:16:38 -0400

    Android push notifications working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,7 +11,42 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+  //StreamSubscription iosSubscrption;
+
+  @override
+  void initState() {
+    super.initState();
+    // if (Platform.isIOS) {
+    //   iosSubscrption = _fcm.onIosSettingsRegistered.listen((data) {
+    //     _saveDeviceToken();
+    //   });
+    //   _fcm.requestNotificationPermissions(IosNotificationSettings());
+    // } else {
+    //   _saveDeviceToken();
+    // }
+
+    _fcm.configure(onMessage: (Map<String, dynamic> message) async {
+      print("onMessage: $message");
+    }, onLaunch: (Map<String, dynamic> message) async {
+      print("onLaunch: $message");
+    }, onResume: (Map<String, dynamic> message) async {
+      print("onResume: $message");
+    });
+    _saveDeviceToken();
+  }
+
+  _saveDeviceToken() async {
+    String fcmToken = await _fcm.getToken();
+    print("DEVICE TOKEN: $fcmToken");
+  }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
